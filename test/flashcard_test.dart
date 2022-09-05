7f099853ea37623cccde3915b7d17c67f6344880
Nick Alekseev <tyumenpost@gmail.com>
2022-09-05 13:17:54 +0500

    added a test

--- test/flashcard_test.dart
+++ test/flashcard_test.dart
@@ -1,7 +1,12 @@
+import 'package:flashcards/view_model/flashcard_manager.dart';
+import 'package:flutter/material.dart';
 import 'package:test/test.dart';
 import 'package:flashcards/model/flashcard.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   group('class Flashcard', () {
     test(
       'String should be returned',
@@ -29,5 +34,29 @@ void main() {
         'Flashcard(frontSide: cat, backSide: кошка, isFavorite: false)',
       );
     });
+  });
+
+  group('class FlashcardManager', () {
+
+    test(
+      'A new flashcard should be created',
+      () {
+        final flashcardManager = FlashcardManager();
+
+        flashcardManager
+          ..isCreatingNewFlashcard = true
+          ..selectedCollectionIndex = 0;
+
+        // Tested method.
+        flashcardManager.createNewFlashcard(
+            frontSide: 'frontTest', backSide: 'backTest');
+
+        expect(
+            flashcardManager.flashcardsRepository.collections[0].flashcards
+                .firstWhere((e) => e.frontSide == 'frontTest')
+                .toString(),
+            'Flashcard(frontSide: frontTest, backSide: backTest, isFavorite: false)');
+      },
+    );
   });
 }
